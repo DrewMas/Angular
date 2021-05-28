18aad645a539abfcb296db501bda2a474e31dbe1
DrewMas <andrewmasnyi@gmail.com>
2021-05-28 15:07:38 +0300

    CW1 - users + posts

---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Post} from "../modules/Post";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  private url = 'http://jsonplaceholder.typicode.com/posts'
+
+  constructor(private httpClient: HttpClient) { }
+
+  getPosts(): Observable<Post[]>{
+    return this.httpClient.get<Post[]>(this.url)
+  }
+}
