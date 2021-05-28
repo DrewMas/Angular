a1d6b699ead7e0895ea49bcdfbcbee42d1b1eb89
DrewMas <andrewmasnyi@gmail.com>
2021-05-28 15:06:39 +0300

    CW1 - users + posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import {HttpClientModule} from "@angular/common/http";
+import { UserComponent } from './components/user/user.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserComponent,
+    PostsComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
